b3bbdf912f85a866a7662d1dd90c35ec8766fba4
esteban <juanestebanmt@gmail.com>
2014-10-18 20:01:46 +1100

    assignment question 2

---
+++ R_programming/assigment1/complete.R
@@ -0,0 +1,28 @@
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  
+  files = paste(directory, "/", sprintf("%03d", id), ".csv", sep="")
+  
+  nobs = c()
+  ids = c()
+  
+  for(file in files){
+    x = read.csv(file)
+    nobs = append(nobs, length(x[complete.cases(x$sulfate)&complete.cases(x$nitrate),][,1]), length(nobs))
+    ids = append(ids, x$ID[1], length(ids))  
+  }
+
+  data.frame(id=ids, nobs=nobs)
+}
