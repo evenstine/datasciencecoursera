1be8a2001443b93b954f9a0d6548406a5a70c18c
esteban <juanestebanmt@gmail.com>
2014-10-18 18:38:54 +1100

    solution to the first question

--- R_programming/assigment1/pollutantmean.R
+++ R_programming/assigment1/pollutantmean.R
@@ -1,4 +1,4 @@
-pollutantmean pollutantmean <- function(directory, pollutant, id = 1:332) {
+pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
 
@@ -11,4 +11,20 @@ pollutantmean pollutantmean <- function(directory, pollutant, id = 1:332) {
 
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
+  
+        Sum = 0.0
+        count = 0.0
+        
+        files = paste(directory, "/", sprintf("%03d", id), ".csv", sep="")
+        
+        for (file in files){
+          x = read.csv(file)
+          
+          data = x[[pollutant]][!is.na(x[[pollutant]])]
+          
+          Sum = Sum + sum(data)
+          count = count + length(data)
+        }
+        
+        Sum / count
 }
